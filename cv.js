3b46607d857055772c676ed8c9a57e74785f0100
AnnAminA <annetabry@gmail.com>
2018-07-01 19:44:53 +0200

    ajout smooth scrolling

--- cv.js
+++ cv.js
@@ -1,37 +1,40 @@
  $(function() {
 
-/*
-        $('.js-scrollTo').on('click', function() { // Au clic sur un élément
-            var page = $(this).attr('href'); // Page cible
-            var speed = 750; // Durée de l'animation (en ms)
-            $('html, body').animate({ scrollTop: $(page).offset().top }, speed); // Go
-            return false;
-        });
+// _________fonction smooth scrolling__________________
 
-*/
-
-
-
-
-/* Every time the window is scrolled ... */
-        $(window).scroll(function() {
-
-/* Check the location of each desired element */
-        $('.hideme').each(function(i) {
-
-                var bottom_of_object = $(this).position().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-/* If the object is completely visible in the window, fade it it */
-                if (bottom_of_window > bottom_of_object) {
-
-                    $(this).animate({ 'opacity': '1' }, 1500);
-
-                }
-
-            });
-
-        });
+     $('a[href^="#"]').on('click', function() {
+         var the_id = $(this).attr("href");
+         if (the_id === '#') {
+             return;
+         }
+         $('html, body').animate({
+             scrollTop: $(the_id).offset().top
+         }, 'slow');
+         return false;
+     });
 
 
-    });
+// ___________fonction fade slow_____________
+
+     /* Every time the window is scrolled ... */
+     $(window).scroll(function() {
+
+         /* Check the location of each desired element */
+         $('.hideme').each(function(i) {
+
+             var bottom_of_object = $(this).position().top + $(this).outerHeight();
+             var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+             /* If the object is completely visible in the window, fade it it */
+             if (bottom_of_window > bottom_of_object) {
+
+                 $(this).animate({ 'opacity': '1' }, 800);
+
+             }
+
+         });
+
+     });
+
+
+ });
